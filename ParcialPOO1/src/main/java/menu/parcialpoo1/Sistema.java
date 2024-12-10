11145ae26c39a1494bf99eabf8350c79538f0764
jdiazse <142941680+jdiazse@users.noreply.github.com>
2024-12-10 15:57:38 -0500

    Update Sistema.java

--- ParcialPOO1/src/main/java/menu/parcialpoo1/Sistema.java
+++ ParcialPOO1/src/main/java/menu/parcialpoo1/Sistema.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package menu.parcialpoo1;
 
-/**
- *
- * @author Estudiante
- */
+//Abstract: Parcial POO
+//Author: Jhon Diaz
+
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
